e7ddcc927d108dff4f75e74e15d9da177b7853a1
Ana Cobian <ana.naiboc@gmail.com>
2022-04-07 10:56:10 -0400

    details of this experiment

--- 2022-04-06/plate_to_color3.R
+++ 2022-04-06/plate_to_color3.R
@@ -15,9 +15,9 @@ kk <- apply(j_table,1, function(x){
   main_t <- paste0(x["row_E_sample"],' group: ',x["GROUP"])
   if(! file.exists(tablepath)) stop(paste0('Error -- tableFile ' , tablepath , ' file not found.'))
   raw_table <- read.table(tablepath,header = TRUE)
-  rownames(raw_table) <- c("A:standard","B:HIV,HIV,HIV+","C:HIV,BRISA,HIV+","D:Blank",
-                 "E:BRISA, BRISA, RV+","F:BRISA,HIV,RV+","G:BRISA, BRISA, RV+","BRISA,HIV,RV+")
-  colnames(raw_table) <- c("competitor 1 ug","competitor 1/2 ug","competitor 1/4 ug","competitor 1/8 ug","competitor 1/16 ug","competitor 1/32 ug","competitor 1/64 ug","competitor 1/128 ug","competitor 1/256 ug","competitor 1/512 ug", "competitor 1/1024 ug","no competitor")
+  rownames(raw_table) <- c("A:standard","B:HIV,HIV,HIV+serum","C:HIV,BRISA,HIV+serum","D:Blank",
+                 "E:BRISA,BRISA,serum","F:BRISA,HIV,serum","G:VIENTO,VIENTO,serum","VIENTO,HIV,serum")
+  colnames(raw_table) <- c("competitor 4 ug","competitor 2 ug","competitor 1 ug","competitor 1/2 ug","competitor 1/4 ug","competitor 1/8 ug","competitor 1/16 ug","competitor 1/32 ug","competitor 1/64 ug","competitor 1/128 ug", "competitor 1/256 ug","no competitor")
   raw_table %>% 
     rownames_to_column(var="antigen") %>%
     pivot_longer(`competitor 1 ug`:`no competitor`,names_to = 'dilutions') %>%
@@ -25,9 +25,9 @@ kk <- apply(j_table,1, function(x){
 })
 
 all_data <- Reduce(rbind,kk) %>%
-  mutate(antigen=factor(antigen,rev(c("A:standard","B:HIV,HIV,HIV+","C:HIV,BRISA,HIV+","D:Blank",
-                                       "E:BRISA, BRISA, RV+","F:BRISA,HIV,RV+","G:BRISA, BRISA, RV+","BRISA,HIV,RV+")))) %>% 
-  mutate(dilutions=factor(dilutions, c("competitor 1 ug","competitor 1/2 ug","competitor 1/4 ug","competitor 1/8 ug","competitor 1/16 ug","competitor 1/32 ug","competitor 1/64 ug","competitor 1/128 ug","competitor 1/256 ug","competitor 1/512 ug", "competitor 1/1024 ug","no competitor")))
+  mutate(antigen=factor(antigen,rev(c("A:standard","B:HIV,HIV,HIV+serum","C:HIV,BRISA,HIV+serum","D:Blank",
+                                      "E:BRISA,BRISA,serum","F:BRISA,HIV,serum","G:VIENTO,VIENTO,serum","VIENTO,HIV,serum")))) %>% 
+  mutate(dilutions=factor(dilutions, c("competitor 4 ug","competitor 2 ug","competitor 1 ug","competitor 1/2 ug","competitor 1/4 ug","competitor 1/8 ug","competitor 1/16 ug","competitor 1/32 ug","competitor 1/64 ug","competitor 1/128 ug", "competitor 1/256 ug","no competitor")))
                   
 
 IgG_plot <- all_data %>%
