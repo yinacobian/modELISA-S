cc137fc4ac6b55643a5b1cd4d1ad1ec4d96bfa65
Ana Cobian <ana.naiboc@gmail.com>
2022-04-07 14:40:02 +0000

    plots

--- 2022-03-15/split_tables2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p table_parts
-ls *txt | xargs -I{} -t sh -c ' FILE="{}"; iconv -f utf-16 -t utf-8 "{}" | grep -A 9 "Plate" | cut -f3-15 | grep -v "^--$" | sed "s/\s*$//gi" | grep -v "^--$" | grep -v PlateFormat |split -l 9 --numeric-suffixes=1 --additional-suffix="_${FILE%.*}.tab" - "table_parts/"'
